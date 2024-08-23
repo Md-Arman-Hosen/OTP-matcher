442208361f2a0ca5c888a4a5549f61e0f01f2f5d
MD-Arman-Hosen <arman15-3304@diu.edu.bd>
2024-08-23 12:16:00 +0600

    final commit

---
+++ js/style.js
@@ -0,0 +1,82 @@
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+        // console.log('pin not 3 digit found', pin);
+        return getPin();
+    }
+}
+
+function generatePin(){
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click', function(){
+    const pin = getPin();
+    // display Pin
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+});
+
+document.getElementById('calculator').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if(isNaN(number)){
+        if(number === 'C'){
+            typedNumberField.value = '';
+        }
+        else if (number === '<'){
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+})
+
+document.getElementById('verify-pin').addEventListener('click', function(){
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const typedNumber = typedNumberField.value;
+    typedNumberField.value = '';
+
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailureMessage = document.getElementById('pin-failure');
+
+    if(typedNumber === currentPin){
+        pinSuccessMessage.style.display = 'block';
+        pinFailureMessage.style.display = 'none';
+    }
+    else{
+        pinFailureMessage.style.display = 'block';
+        pinSuccessMessage.style.display = 'none';
+
+        const actionLeft = document.getElementById('action-left');
+        const actionLeftString = actionLeft.innerText;
+        const actionLeftNumber = parseInt(actionLeftString);
+
+        const newActionLeftNumber = actionLeftNumber-1;
+        actionLeft.innerText = newActionLeftNumber;
+
+        if( newActionLeftNumber == 0)
+        {
+            const btn = document.getElementById('verify-pin');
+            btn.style.display = 'none';
+
+            const typedNumbers  = document.getElementById('typed-numbers');
+            typedNumbers.style.display = 'none';
+        }
+
+    }
+})
